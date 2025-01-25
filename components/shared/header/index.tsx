3f0516208e5f34b87fb77c7ad6058136631f9646
Anshul Anand <anandanshul001@gmail.com>
2025-01-25 14:26:44 +0530

    feat: Header & Footer

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -28,7 +28,7 @@ const Header = () => {
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant='ghost'>
+          <Button asChild>
             <Link href={'/sign-in'}>
               <ShoppingCart /> Sign In
             </Link>
